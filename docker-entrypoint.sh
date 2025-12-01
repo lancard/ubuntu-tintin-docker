270fd6019049dd69df6eacd38d1e9e38945da80a
Sung-ho Kim <42758339+lancard@users.noreply.github.com>
2025-12-01 14:16:02 +0900

    Add symbolic links for account files and start services
    Create symbolic links for account files and start services.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+ln -sf /etc-account/passwd /etc/passwd
+ln -sf /etc-account/shadow /etc/shadow
+ln -sf /etc-account/group /etc/group
+ln -sf /etc-account/gshadow /etc/gshadow
+
 service cron start
 service openbsd-inetd start
 service ssh start
