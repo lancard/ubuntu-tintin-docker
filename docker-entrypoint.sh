6974113f09c0a86d5f40b45b3c05052d1b84977f
Sung-ho Kim <42758339+lancard@users.noreply.github.com>
2025-12-01 15:03:29 +0900

    Implement termination handler in entrypoint script
    Added a termination handler for graceful shutdown.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+term_handler() {
+    kill -TERM "$child" 2>/dev/null || true
+    exit 0
+}
+
 ln -sf /etc-account/passwd /etc/passwd
 ln -sf /etc-account/shadow /etc/shadow
 ln -sf /etc-account/group /etc/group
 ln -sf /etc-account/gshadow /etc/gshadow
 
 service cron start
-service openbsd-inetd start
+service inetutils-inetd restart
 service ssh start
 
 echo love > /etc/hostname
 
-tail -f /dev/null
+tail -f /dev/null &
+child=$!
+wait "$child"
